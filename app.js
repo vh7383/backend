7d38017933866a9981224adec5e073a9178cd29e
vh7383 <vincent.hernandes@gmail.com>
2022-06-25 21:53:03 +0200

    implement MongoDB and models

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
 app.use(express.json());
+mongoose.connect('mongodb+srv://vh7383:ak18071206@cluster0.ldfjl.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
