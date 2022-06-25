aaa155d2d9b00ffdc62e4c584faf5596e0114eac
vh7383 <vincent.hernandes@gmail.com>
2022-06-25 21:13:05 +0200

    implement POST endpoint

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.post('/api/stuff', (req, res) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé !'
+    });
 });
 
 app.use('/api/stuff', (req, res) => {
@@ -30,5 +38,7 @@ app.use('/api/stuff', (req, res) => {
     ];
     res.status(200).json(stuff);
 });
+
+
 
 module.exports = app;
