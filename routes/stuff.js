ad7da80fc37a02515954b424f64b4af9607581c1
vh7383 <vincent.hernandes@gmail.com>
2022-06-28 14:37:21 +0200

    implement auth middleware

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', stuffCtrl.createThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', );
-router.delete('/:id', stuffCtrl.deleteThing);
-router.get('/', stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/:id', auth,  stuffCtrl.getOneThing);
+router.put('/:id', auth,  );
+router.delete('/:id', auth,  stuffCtrl.deleteThing);
+router.get('/', auth,  stuffCtrl.getAllStuff);
 
 module.exports = router;
